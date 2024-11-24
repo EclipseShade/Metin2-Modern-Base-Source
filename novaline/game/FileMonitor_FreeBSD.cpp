9efbcf1f73c35bd1543051d3703c10571da099ba
Metin2 <priv.shadow.project@gmail.com>
2024-11-24 12:52:16 +0100

    .

--- novaline/game/FileMonitor_FreeBSD.cpp
+++ novaline/game/FileMonitor_FreeBSD.cpp
@@ -1,7 +1,8 @@
 #include "stdafx.h"
-#include "FileMonitor_FreeBSD.h"
-#include "../../libthecore/include/log.h"
 
+#include "../../ExternGame/libthecore/include/log.h"
+
+#include "FileMonitor_FreeBSD.h"
 
 #define INVALID_KERNEL_EVENT	-1
 
