92afe18e73b4858e2775fb2415a38cd4ef6dc655
Metin2 <priv.shadow.project@gmail.com>
2024-11-28 11:40:43 +0100

    *removed any_function.inc -> any_function.h
    *we removed .inc and make two boost to avoid conflict etc

--- mainline/game/any_function.h
+++ mainline/game/any_function.h
@@ -14,20 +14,252 @@
 #define boost _boost_func_of_SQLMsg
 #define func_arg_type SQLMsg*
 #define func_arg pmsg
-#include "any_function.inc"
+
+namespace boost {
+    class any
+    {
+    public: // structors
+
+        any()
+          : content(0)
+        {
+        }
+
+        template<typename ValueType>
+        any(const ValueType & value)
+          : content(new holder<ValueType>(value))
+        {
+        }
+
+        any(const any & other)
+          : content(other.content ? other.content->clone() : 0)
+        {
+        }
+
+        ~any()
+        {
+            delete content;
+        }
+
+    public: // modifiers
+
+        any & swap(any & rhs)
+        {
+            std::swap(content, rhs.content);
+            return *this;
+        }
+
+        template<typename ValueType>
+        any & operator=(const ValueType & rhs)
+        {
+            any(rhs).swap(*this);
+            return *this;
+        }
+
+        any & operator=(const any & rhs)
+        {
+            any(rhs).swap(*this);
+            return *this;
+        }
+
+	void operator ()(func_arg_type func_arg)
+	{
+	    (*content)(func_arg);
+	}
+
+    public: // queries
+
+        bool empty() const
+        {
+            return !content;
+        }
+
+    private: // types
+
+        class placeholder
+        {
+        public: // structors
+    
+            virtual ~placeholder()
+            {
+            }
+
+        public: // queries
+
+            virtual placeholder * clone() const = 0;
+
+	    virtual void operator()(func_arg_type func_arg) = 0;
+    
+        };
+
+        template<typename ValueType>
+        class holder : public placeholder
+        {
+        public: // structors
+
+            holder(const ValueType & value)
+              : held(value)
+            {
+            }
+
+        public: // queries
+
+            virtual placeholder * clone() const
+            {
+                return new holder(held);
+            }
+
+	    virtual void operator ()(func_arg_type func_arg)
+	    {
+		held(func_arg);
+	    }
+
+        public: // representation
+
+            ValueType held;
+
+        };
+
+    private: // representation
+
+        template<typename ValueType>
+        friend ValueType * any_cast(any *);
+
+        placeholder * content;
+
+    };
+
+}
+
 #undef func_arg
 #undef func_arg_type
 #undef boost
 
 typedef _boost_func_of_SQLMsg::any any_function;
 
-#define boost _boost_func_of_void
-#define func_arg_type 
-#define func_arg 
-#include "any_function.inc"
-#undef func_arg
-#undef func_arg_type
-#undef boost
+#define boost2 _boost_func_of_void
+#define func_arg_type2
+#define func_arg2
+
+namespace boost2 {
+    class any
+    {
+    public: // structors
+
+        any()
+          : content(0)
+        {
+        }
+
+        template<typename ValueType>
+        any(const ValueType & value)
+          : content(new holder<ValueType>(value))
+        {
+        }
+
+        any(const any & other)
+          : content(other.content ? other.content->clone() : 0)
+        {
+        }
+
+        ~any()
+        {
+            delete content;
+        }
+
+    public: // modifiers
+
+        any & swap(any & rhs)
+        {
+            std::swap(content, rhs.content);
+            return *this;
+        }
+
+        template<typename ValueType>
+        any & operator=(const ValueType & rhs)
+        {
+            any(rhs).swap(*this);
+            return *this;
+        }
+
+        any & operator=(const any & rhs)
+        {
+            any(rhs).swap(*this);
+            return *this;
+        }
+
+	void operator ()(func_arg_type2 func_arg2)
+	{
+	    (*content)(func_arg2);
+	}
+
+    public: // queries
+
+        bool empty() const
+        {
+            return !content;
+        }
+
+    private: // types
+
+        class placeholder
+        {
+        public: // structors
+    
+            virtual ~placeholder()
+            {
+            }
+
+        public: // queries
+
+            virtual placeholder * clone() const = 0;
+
+	    virtual void operator()(func_arg_type2 func_arg2) = 0;
+    
+        };
+
+        template<typename ValueType>
+        class holder : public placeholder
+        {
+        public: // structors
+
+            holder(const ValueType & value)
+              : held(value)
+            {
+            }
+
+        public: // queries
+
+            virtual placeholder * clone() const
+            {
+                return new holder(held);
+            }
+
+	    virtual void operator ()(func_arg_type2 func_arg2)
+	    {
+		held(func_arg2);
+	    }
+
+        public: // representation
+
+            ValueType held;
+
+        };
+
+    private: // representation
+
+        template<typename ValueType>
+        friend ValueType * any_cast(any *);
+
+        placeholder * content;
+
+    };
+
+}
+
+#undef func_arg2
+#undef func_arg_type2
+#undef boost2
 
 typedef _boost_func_of_void::any any_void_function;
 
@@ -51,14 +283,6 @@ inline void_binder<F> void_bind(const F& f, const Arg& arg)
 	typedef typename F::argument_type arg_type;
 	return void_binder<F>(f, arg_type(arg));
 }
-
-// Copyright Kevlin Henney, 2000, 2001, 2002. All rights reserved.
-//
-// Permission to use, copy, modify, and distribute this software for any
-// purpose is hereby granted without fee, provided that this copyright and
-// permissions notice appear in all copies and derivatives.
-//
-// This software is provided "as is" without express or implied warranty.
 
 #endif
 
