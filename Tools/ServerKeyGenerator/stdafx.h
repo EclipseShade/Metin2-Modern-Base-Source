fca68b5e99436804dc34f43de28fce8d26354205
EclipseShade <priv.shadow.project@gmail.com>
2024-11-29 14:15:23 +0100

    Update stdafx.h

--- Tools/ServerKeyGenerator/stdafx.h
+++ Tools/ServerKeyGenerator/stdafx.h
@@ -1,13 +1,14 @@
 #ifndef __SERVER_KEY_GENERATOR_STDAFX__H
 #define __SERVER_KEY_GENERATOR_STDAFX__H
 
-#include <fstream>
-#include <sstream>
-
-
+#include <winsock2.h>
 #include <windows.h>
 #include <tchar.h>
 #include <stdio.h>
 #include <atlenc.h>
+
+// STANDARD LIBRARIES
+#include <fstream>
+#include <sstream>
 
 #endif
