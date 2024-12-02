7818d30e55430f4843ff8ae5d8b075e04c91ccaf
EclipseShade <priv.shadow.project@gmail.com>
2024-12-02 08:31:03 +0100

    *unused

--- Extern/ExternGame/libthecore/include/memcpy.h
+++ Extern/ExternGame/libthecore/include/memcpy.h
@@ -5,9 +5,6 @@
 extern "C"
 {
 #endif
-#ifdef __LIBTHECORE__
-    void thecore_find_best_memcpy();
-#endif
 
 #ifndef __WIN32__
     extern void *(*thecore_memcpy) (void * to, const void * from, size_t len);
