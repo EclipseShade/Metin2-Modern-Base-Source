9e2fc0b2dd290a8a2598b614298f633ddc337d27
EclipseShade <priv.shadow.project@gmail.com>
2024-12-02 13:59:36 +0100

    Update ClientManagerEventFlag.cpp

--- mainline/db/ClientManagerEventFlag.cpp
+++ mainline/db/ClientManagerEventFlag.cpp
@@ -6,67 +6,69 @@
 #include "DBManager.h"
 #include "QID.h"
 
-void CClientManager::LoadEventFlag()
-{
-	char szQuery[1024];
-	snprintf(szQuery, sizeof(szQuery), "SELECT szName, lValue FROM quest%s WHERE dwPID = 0", GetTablePostfix());
-	std::auto_ptr<SQLMsg> pmsg(CDBManager::instance().DirectQuery(szQuery));
+void CClientManager::LoadEventFlag() {
+    std::ostringstream queryStream;
+    queryStream << "SELECT szName, lValue FROM quest" << GetTablePostfix() << " WHERE dwPID = 0";
+    
+    std::auto_ptr<SQLMsg> pmsg(CDBManager::instance().DirectQuery(queryStream.str().c_str()));
 
-	SQLResult* pRes = pmsg->Get();
-	if (pRes->uiNumRows)
-	{
-		MYSQL_ROW row;
-		while ((row = mysql_fetch_row(pRes->pSQLResult)))
-		{
-			TPacketSetEventFlag p;
-			strlcpy(p.szFlagName, row[0], sizeof(p.szFlagName));
-			str_to_number(p.lValue, row[1]);
-			sys_log(0, "EventFlag Load %s %d", p.szFlagName, p.lValue);
-			m_map_lEventFlag.insert(std::make_pair(std::string(p.szFlagName), p.lValue));
-			ForwardPacket(HEADER_DG_SET_EVENT_FLAG, &p, sizeof(TPacketSetEventFlag));
-		}
-	}
+    SQLResult* pRes = pmsg->Get();
+    if (pRes->uiNumRows) {
+        MYSQL_ROW row;
+        while ((row = mysql_fetch_row(pRes->pSQLResult))) {
+            TPacketSetEventFlag p;
+            strlcpy(p.szFlagName, row[0], sizeof(p.szFlagName));
+            str_to_number(p.lValue, row[1]);
+            
+            std::ostringstream logStream;
+            logStream << "EventFlag Load " << p.szFlagName << " " << p.lValue;
+            sys_log(0, logStream.str().c_str());
+            
+            m_map_lEventFlag.insert(std::make_pair(std::string(p.szFlagName), p.lValue));
+            ForwardPacket(HEADER_DG_SET_EVENT_FLAG, &p, sizeof(TPacketSetEventFlag));
+        }
+    }
 }
 
-void CClientManager::SetEventFlag(TPacketSetEventFlag* p)
-{
-	ForwardPacket(HEADER_DG_SET_EVENT_FLAG, p, sizeof(TPacketSetEventFlag));
+void CClientManager::SetEventFlag(TPacketSetEventFlag* p) {
+    ForwardPacket(HEADER_DG_SET_EVENT_FLAG, p, sizeof(TPacketSetEventFlag));
 
-	bool bChanged = false;
+    bool bChanged = false;
 
-	typeof(m_map_lEventFlag.begin()) it = m_map_lEventFlag.find(p->szFlagName);
-	if (it == m_map_lEventFlag.end())
-	{
-		bChanged = true;
-		m_map_lEventFlag.insert(std::make_pair(std::string(p->szFlagName), p->lValue));
-	}
-	else if (it->second != p->lValue)
-	{
-		bChanged = true;
-		it->second = p->lValue;
-	}
+    auto it = m_map_lEventFlag.find(p->szFlagName);
+    if (it == m_map_lEventFlag.end()) {
+        bChanged = true;
+        m_map_lEventFlag.insert(std::make_pair(std::string(p->szFlagName), p->lValue));
+    } else if (it->second != p->lValue) {
+        bChanged = true;
+        it->second = p->lValue;
+    }
 
-	if (bChanged)
-	{
-		char szQuery[1024];
-		snprintf(szQuery, sizeof(szQuery),
-				"REPLACE INTO quest%s (dwPID, szName, szState, lValue) VALUES(0, '%s', '', %ld)",
-				GetTablePostfix(), p->szFlagName, p->lValue);
-		szQuery[1023] = '\0';
+    if (bChanged) {
+        std::ostringstream oss;
+        oss << "REPLACE INTO quest" << GetTablePostfix()
+            << " (dwPID, szName, szState, lValue) VALUES(0, '"
+            << p->szFlagName << "', '', " << p->lValue << ")";
 
-		//CDBManager::instance().ReturnQuery(szQuery, QID_QUEST_SAVE, 0, NULL);
-		CDBManager::instance().AsyncQuery(szQuery);
-		sys_log(0, "HEADER_GD_SET_EVENT_FLAG : Changed CClientmanager::SetEventFlag(%s %d) ", p->szFlagName, p->lValue);
-		return;
-	}
-	sys_log(0, "HEADER_GD_SET_EVENT_FLAG : No Changed CClientmanager::SetEventFlag(%s %d) ", p->szFlagName, p->lValue);
+        CDBManager::instance().AsyncQueryPrepare(szQuery.str());
+
+        std::ostringstream logOss;
+        logOss << "HEADER_GD_SET_EVENT_FLAG : Changed CClientmanager::SetEventFlag("
+               << p->szFlagName << " " << p->lValue << ") ";
+        sys_log(0, logOss.str().c_str());
+        return;
+    }
+
+    std::ostringstream logOss;
+    logOss << "HEADER_GD_SET_EVENT_FLAG : No Changed CClientmanager::SetEventFlag("
+           << p->szFlagName << " " << p->lValue << ") ";
+    sys_log(0, logOss.str().c_str());
 }
 
-void CClientManager::SendEventFlagsOnSetup(CPeer* peer)
-{
+void CClientManager::SendEventFlagsOnSetup(CPeer* peer) {
 	typeof(m_map_lEventFlag.begin()) it;
-	for (it = m_map_lEventFlag.begin(); it != m_map_lEventFlag.end(); ++it)
-	{
+
+	for (it = m_map_lEventFlag.begin(); it != m_map_lEventFlag.end(); ++it) {
 		TPacketSetEventFlag p;
 		strlcpy(p.szFlagName, it->first.c_str(), sizeof(p.szFlagName));
 		p.lValue = it->second;
