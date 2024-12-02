c95c9106cad1745066101bea7760392320279a9b
EclipseShade <priv.shadow.project@gmail.com>
2024-12-02 14:00:19 +0100

    Update ClientManagerHorseName.cpp

--- mainline/db/ClientManagerHorseName.cpp
+++ mainline/db/ClientManagerHorseName.cpp
@@ -2,39 +2,33 @@
 
 #include "ClientManager.h"
 
-void CClientManager::UpdateHorseName(TPacketUpdateHorseName* data, CPeer* peer)
-{
-	char szQuery[512];
+void CClientManager::UpdateHorseName(TPacketUpdateHorseName* data, CPeer* peer) {
+    std::ostringstream queryHorseName;
 
-	snprintf(szQuery, sizeof(szQuery), "REPLACE INTO horse_name VALUES(%u, '%s')", data->dwPlayerID, data->szHorseName);
+    queryHorseName << "REPLACE INTO horse_name VALUES(" << data->dwPlayerID << ", '"
+        << data->szHorseName << "')";
 
-	std::auto_ptr<SQLMsg> pmsg_insert(CDBManager::instance().DirectQuery(szQuery));
+    std::auto_ptr<SQLMsg> pmsg_insert(CDBManager::instance().DirectQuery(queryHorseName.str().c_str()));
 
-	ForwardPacket(HEADER_DG_UPDATE_HORSE_NAME, data, sizeof(TPacketUpdateHorseName), 0, peer);
+    ForwardPacket(HEADER_DG_UPDATE_HORSE_NAME, data, sizeof(TPacketUpdateHorseName), 0, peer);
 }
 
-void CClientManager::AckHorseName(DWORD dwPID, CPeer* peer)
-{
-	char szQuery[512];
+void CClientManager::AckHorseName(DWORD dwPID, CPeer* peer) {
+    std::ostringstream queryHorseName;
+    queryHorseName << "SELECT name FROM horse_name WHERE id = " << dwPID;
 
-	snprintf(szQuery, sizeof(szQuery), "SELECT name FROM horse_name WHERE id = %u", dwPID);
+    std::auto_ptr<SQLMsg> pmsg(CDBManager::instance().DirectQuery(queryHorseName.str().c_str()));
 
-	std::auto_ptr<SQLMsg> pmsg(CDBManager::instance().DirectQuery(szQuery));
+    TPacketUpdateHorseName packet;
+    packet.dwPlayerID = dwPID;
 
-	TPacketUpdateHorseName packet;
-	packet.dwPlayerID = dwPID;
+    if (pmsg->Get()->uiNumRows == 0) {
+        memset(packet.szHorseName, 0, sizeof(packet.szHorseName));
+    } else {
+        MYSQL_ROW row = mysql_fetch_row(pmsg->Get()->pSQLResult);
+        strlcpy(packet.szHorseName, row[0], sizeof(packet.szHorseName));
+    }
 
-	if (pmsg->Get()->uiNumRows == 0)
-	{
-		memset(packet.szHorseName, 0, sizeof (packet.szHorseName));
-	}
-	else
-	{
-		MYSQL_ROW row = mysql_fetch_row(pmsg->Get()->pSQLResult);
-		strlcpy(packet.szHorseName, row[0], sizeof(packet.szHorseName));
-	}
-
-	peer->EncodeHeader(HEADER_DG_ACK_HORSE_NAME, 0, sizeof(TPacketUpdateHorseName));
-	peer->Encode(&packet, sizeof(TPacketUpdateHorseName));
+    peer->EncodeHeader(HEADER_DG_ACK_HORSE_NAME, 0, sizeof(TPacketUpdateHorseName));
+    peer->Encode(&packet, sizeof(TPacketUpdateHorseName));
 }
-
