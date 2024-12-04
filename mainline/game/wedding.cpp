be92344a1843fc78ce62b7f20c95fa8e1f9e1df7
EclipseShade <priv.shadow.project@gmail.com>
2024-12-04 19:39:11 +0100

    Update wedding.cpp

--- mainline/game/wedding.cpp
+++ mainline/game/wedding.cpp
@@ -35,13 +35,13 @@ namespace marriage
 			sys_err( "wedding_end_event> <Factor> Null pointer" );
 			return 0;
 		}
-		
+
 		WeddingMap* pMap = info->pWeddingMap;
 
 		if (info->iStep == 0)
 		{
 			++info->iStep;
-			pMap->WarpAll(); 
+			pMap->WarpAll();
 			return PASSES_PER_SEC(15);
 		}
 		WeddingManager::instance().DestroyWeddingMap(pMap);
@@ -88,7 +88,7 @@ namespace marriage
 			if (ch->GetPlayerID() == dwPID1 || ch->GetPlayerID() == dwPID2)
 				continue;
 
-			if (ch->GetLevel() < 10) // 10 레벨이하는 주지않는다.
+			if (ch->GetLevel() < 10)
 				continue;
 
 			//ch->AutoGiveItem(27003, 5);
@@ -123,9 +123,6 @@ namespace marriage
 		{
 			if (ch->IsPC())
 			{
-				// ExitToSavedLocation은 WarpSet을 부르는데 이 함수에서
-				// Sectree가 NULL이 된다. 추 후 SectreeManager로 부터는
-				// 이 캐릭터를 찾을 수 없으므로 아래 DestroyAll에서 별도 처리함
 				ch->ExitToSavedLocation();
 			}
 		}
@@ -153,7 +150,7 @@ namespace marriage
 	void WeddingMap::DestroyAll()
 	{
 		sys_log(0, "WeddingMap::DestroyAll: m_set_pkChr size %zu", m_set_pkChr.size());
-		
+
 		FDestroyEveryone f;
 
 		for (charset_t::iterator it = m_set_pkChr.begin(); it != m_set_pkChr.end(); it = m_set_pkChr.begin())
@@ -223,7 +220,7 @@ namespace marriage
 			{
 				ShoutInMap(CHAT_TYPE_COMMAND, __BuildCommandPlayMusic(szCommand, sizeof(szCommand), 0, "default"));
 			}
-		} 
+		}
 	}
 
 	void WeddingMap::SetDark(bool bSet)
@@ -266,7 +263,7 @@ namespace marriage
 		if (m_isSnow)
 			ch->ChatPacket(CHAT_TYPE_COMMAND, "xmas_snow 1");
 		if (m_isMusic)
-			ch->ChatPacket(CHAT_TYPE_COMMAND, __BuildCommandPlayMusic(szCommand, sizeof(szCommand), 1, m_stMusicFileName.c_str()));	
+			ch->ChatPacket(CHAT_TYPE_COMMAND, __BuildCommandPlayMusic(szCommand, sizeof(szCommand), 1, m_stMusicFileName.c_str()));
 	}
 
 	const char* WeddingMap::__BuildCommandPlayMusic(char* szCommand, size_t nCmdLen, BYTE bSet, const char* c_szMusicFileName)
@@ -319,7 +316,6 @@ namespace marriage
 
 		m_mapWedding.insert(make_pair(dwMapIndex, M2_NEW WeddingMap(dwMapIndex, dwPID1, dwPID2)));
 
-
 		// LOCALE_SERVICE
 		LPSECTREE_MAP pkSectreeMap = rkSecTreeMgr.GetMap(dwMapIndex);
 		if (pkSectreeMap == NULL) {
@@ -342,7 +338,7 @@ namespace marriage
 
 		return dwMapIndex;
 	}
-	
+
 	void WeddingManager::DestroyWeddingMap(WeddingMap* pMap)
 	{
 		sys_log(0, "DestroyWeddingMap(index=%u)", pMap->GetMapIndex());
@@ -383,5 +379,5 @@ namespace marriage
 			db_clientdesc->DBPacket(HEADER_GD_WEDDING_READY, 0, &p, sizeof(p));
 		}
 	}
-
 }
+
