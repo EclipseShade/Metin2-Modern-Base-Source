33088a479362726662c0a2d3902d2a4c65cbbc1b
EclipseShade <priv.shadow.project@gmail.com>
2024-12-04 14:15:13 +0100

    Update Base.h

--- Extern/ExternGame/libpoly/Base.h
+++ Extern/ExternGame/libpoly/Base.h
@@ -10,7 +10,7 @@
 #define MID_SQRT		MID_NUMBER + 2
 #define MID_FRACTION		MID_NUMBER + 3
 
-class CBase  
+class CBase
 {
     public:
 	bool isSymbol();
@@ -19,7 +19,6 @@ class CBase
 	int id;
 	CBase();
 	virtual ~CBase();
-
 };
 
 #endif
