214cc0941b56cb59ce19b5b7c22e2ba8014fdc89
EclipseShade <priv.shadow.project@gmail.com>
2024-12-04 19:58:33 +0100

    Update shop.h

--- mainline/game/shop.h
+++ mainline/game/shop.h
@@ -14,12 +14,12 @@ class CShop
 	public:
 		typedef struct shop_item
 		{
-			DWORD	vnum;		// 아이템 번호
-			long	price;		// 가격
-			BYTE	count;		// 아이템 개수
+			DWORD	vnum;
+			long	price;
+			BYTE	count;
 
 			LPITEM	pkItem;
-			int		itemid;		// 아이템 고유아이디
+			int		itemid;
 
 			shop_item()
 			{
@@ -34,26 +34,17 @@ class CShop
 		CShop();
 		~CShop();
 
-		bool	Create(DWORD dwVnum, DWORD dwNPCVnum, TShopItemTable * pItemTable);
-		void	SetShopItems(TShopItemTable * pItemTable, BYTE bItemCount);
+		bool			Create(DWORD dwVnum, DWORD dwNPCVnum, TShopItemTable * pItemTable);
+		void			SetShopItems(TShopItemTable * pItemTable, BYTE bItemCount);
 
 		virtual void	SetPCShop(LPCHARACTER ch);
 		virtual bool	IsPCShop()	{ return m_pkPC ? true : false; }
 
-		// 게스트 추가/삭제
 		virtual bool	AddGuest(LPCHARACTER ch,DWORD owner_vid, bool bOtherEmpire);
-		void	RemoveGuest(LPCHARACTER ch);
-
-		// 물건 구입
-		virtual int	Buy(LPCHARACTER ch, BYTE pos);
-
-		// 게스트에게 패킷을 보냄
-		void	BroadcastUpdateItem(BYTE pos);
-
-		// 판매중인 아이템의 갯수를 알려준다.
-		int		GetNumberByVnum(DWORD dwVnum);
-
-		// 아이템이 상점에 등록되어 있는지 알려준다.
+		void			RemoveGuest(LPCHARACTER ch);
+		virtual int		Buy(LPCHARACTER ch, BYTE pos);
+		void			BroadcastUpdateItem(BYTE pos);
+		int				GetNumberByVnum(DWORD dwVnum);
 		virtual bool	IsSellingItem(DWORD itemID);
 
 		DWORD	GetVnum() { return m_dwVnum; }
@@ -70,9 +61,10 @@ class CShop
 
 		typedef TR1_NS::unordered_map<LPCHARACTER, bool> GuestMapType;
 		GuestMapType m_map_guest;
-		std::vector<SHOP_ITEM>		m_itemVector;	// 이 상점에서 취급하는 물건들
+		std::vector<SHOP_ITEM>		m_itemVector;
 
 		LPCHARACTER			m_pkPC;
 };
 
-#endif 
+#endif
+
