f17c2e2062be87f03afa4ad841ab59be0d342343
EclipseShade <priv.shadow.project@gmail.com>
2024-12-04 09:48:21 +0100

    Update HB.cpp

--- mainline/db/HB.cpp
+++ mainline/db/HB.cpp
@@ -21,7 +21,6 @@ bool PlayerHB::Initialize()
 	snprintf(szQuery, sizeof(szQuery), "SHOW CREATE TABLE player%s", GetTablePostfix());
 
 	std::auto_ptr<SQLMsg> pMsg(CDBManager::instance().DirectQuery(szQuery));
-
 	if (pMsg->Get()->uiNumRows == 0)
 		return false;
 
@@ -31,7 +30,7 @@ bool PlayerHB::Initialize()
 }
 
 //
-// @version	05/07/05 Bang2ni - id 에 해당하는 data 가 없을 때 쿼리하고 data 를 insert  하는코드 추가.
+
 //
 void PlayerHB::Put(DWORD id)
 {
@@ -49,14 +48,14 @@ void PlayerHB::Put(DWORD id)
 }
 
 //
-// @version	05/07/05 Bang2ni - Query string 버퍼가 작아서 늘려줌.
+
 //
 bool PlayerHB::Query(DWORD id)
 {
 	time_t ct = time(0);
 	struct tm curr_tm = *localtime(&ct);
 	char szTableName[64];
-	snprintf(szTableName, sizeof(szTableName), "hb_%02d%02d%02d%02d_player%s", 
+	snprintf(szTableName, sizeof(szTableName), "hb_%02d%02d%02d%02d_player%s",
 			curr_tm.tm_year - 100, curr_tm.tm_mon + 1, curr_tm.tm_mday, curr_tm.tm_hour, GetTablePostfix());
 
 	char szQuery[4096];
@@ -69,13 +68,11 @@ bool PlayerHB::Query(DWORD id)
 
 		if (pos < 0)
 		{
-			sys_err("cannot find %s ", szFind);	
-		//	sys_err("cannot find %s in %s", szFind, m_stCreateTableQuery.c_str());
+			sys_err("cannot find %s ", szFind);
 			return false;
 		}
 
 		snprintf(szQuery, sizeof(szQuery), "CREATE TABLE IF NOT EXISTS %s%s", szTableName, m_stCreateTableQuery.c_str() + strlen(szFind));
-	//	sys_log(0, "%s", szQuery);
 		std::auto_ptr<SQLMsg> pMsg(CDBManager::instance().DirectQuery(szQuery, SQL_HOTBACKUP));
 		m_stTableName = szTableName;
 	}
