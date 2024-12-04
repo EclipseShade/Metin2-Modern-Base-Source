af21a51d03ebf8fd76b59f8b43ca1244c5b4bdbb
EclipseShade <priv.shadow.project@gmail.com>
2024-12-04 13:56:29 +0100

    Update singleton.h

--- mainline/common/singleton.h
+++ mainline/common/singleton.h
@@ -3,22 +3,22 @@
 
 #include <assert.h>
 
-template <typename T> class singleton 
-{ 
-	public: 
+template <typename T> class singleton
+{
+	public:
 		static T * ms_singleton;
 
 		singleton()
-		{ 
+		{
 			assert(!ms_singleton);
-			long offset = (long) (T*) 1 - (long) (singleton <T>*) (T*) 1; 
+			long offset = (long) (T*) 1 - (long) (singleton <T>*) (T*) 1;
 			ms_singleton = (T*) ((long) this + offset);
-		} 
+		}
 
 		virtual ~singleton()
-		{ 
+		{
 			assert(ms_singleton);
-			ms_singleton = 0; 
+			ms_singleton = 0;
 		}
 
 		static T & instance()
