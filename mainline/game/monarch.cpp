e051c438fb8dded1634b9f0808ca4aa627883b74
EclipseShade <priv.shadow.project@gmail.com>
2024-12-06 13:07:48 +0100

    Update monarch.cpp

--- mainline/game/monarch.cpp
+++ mainline/game/monarch.cpp
@@ -29,7 +29,7 @@ bool CMonarch::Initialize()
 	return 0;
 }
 
-struct FHealMyEmpire 
+struct FHealMyEmpire
 {
 	BYTE m_bEmpire;
 	void operator () (LPENTITY ent)
@@ -74,7 +74,7 @@ int CMonarch::HealMyEmpire(LPCHARACTER ch ,DWORD price)
 		return 0;
 	}
 
-	if (!IsMoneyOk(price, Empire)) 
+	if (!IsMoneyOk(price, Empire))
 	{
 		ch->ChatPacket(CHAT_TYPE_INFO, LC_TEXT("국고에 돈이 부족합니다. 현재 : %u 필요금액 : %u"), GetMoney(Empire), price);
 		return 0;
@@ -84,12 +84,10 @@ int CMonarch::HealMyEmpire(LPCHARACTER ch ,DWORD price)
 
 	FHealMyEmpire f;
 	f.m_bEmpire = Empire;
-	SECTREE_MANAGER::instance().for_each(iMapIndex, f);	
+	SECTREE_MANAGER::instance().for_each(iMapIndex, f);
 
-	// DB에 돈 삭감 보내기 
 	SendtoDBDecMoney(price, Empire, ch);
 
-	// 쿨타임 설정	
 	ch->SetMC(CHARACTER::MI_HEAL);
 
 	if (test_server)
@@ -112,7 +110,7 @@ bool CMonarch::IsMonarch(DWORD pid, BYTE bEmpire)
 
 bool CMonarch::IsMoneyOk(int price, BYTE bEmpire)
 {
-	dev_log(LOG_DEB1, "GetMoney(%d), price = (%d,%d)", bEmpire, GetMoney(bEmpire), price);
+	sys_log(1, "GetMoney(%d), price = (%d,%d)", bEmpire, GetMoney(bEmpire), price);
 	return (GetMoney(bEmpire) >= price);
 }
 
@@ -124,10 +122,10 @@ bool CMonarch::SendtoDBAddMoney(int Money, BYTE bEmpire, LPCHARACTER ch)
 	if (GetMoney(bEmpire) + Money < 0)
 		return false;
 
-	int nEmpire = bEmpire;	
+	int nEmpire = bEmpire;
 	db_clientdesc->DBPacketHeader(HEADER_GD_ADD_MONARCH_MONEY, ch->GetDesc()->GetHandle(), sizeof(int) + sizeof(int));
-	db_clientdesc->Packet(&nEmpire, sizeof(int));	
-	db_clientdesc->Packet(&Money, sizeof(int));	
+	db_clientdesc->Packet(&nEmpire, sizeof(int));
+	db_clientdesc->Packet(&Money, sizeof(int));
 	return true;
 }
 
@@ -139,12 +137,11 @@ bool CMonarch::SendtoDBDecMoney(int Money, BYTE bEmpire, LPCHARACTER ch)
 	if (GetMoney(bEmpire) - Money < 0)
 		return false;
 
-	// 실제 줄이는 부분은 서버에 갔다 온 다음에 처리된다
-	int nEmpire = bEmpire;	
+	int nEmpire = bEmpire;
 
 	db_clientdesc->DBPacketHeader(HEADER_GD_DEC_MONARCH_MONEY, ch->GetDesc()->GetHandle(), sizeof(int) + sizeof(int));
-	db_clientdesc->Packet(&nEmpire, sizeof(int));	
-	db_clientdesc->Packet(&Money, sizeof(int));	
+	db_clientdesc->Packet(&nEmpire, sizeof(int));
+	db_clientdesc->Packet(&Money, sizeof(int));
 	return true;
 }
 
@@ -153,7 +150,6 @@ bool CMonarch::AddMoney(int Money, BYTE bEmpire)
 	if (bEmpire >= _countof(m_MonarchInfo.money))
 		return false;
 
-	// 20억 이상 입금 불가능
 	if (GetMoney(bEmpire) + Money > 2000000000)
 		return false;
 
@@ -238,7 +234,6 @@ void CMonarch::PowerUp(BYTE Empire, bool On)
 
 	m_PowerUp[Empire] = On;
 
-	// 군주 사자후 쿨타임 
 	m_PowerUpCT[Empire] = thecore_pulse() + PASSES_PER_SEC(60 * 10);
 }
 
@@ -249,13 +244,11 @@ void CMonarch::DefenseUp(BYTE Empire, bool On)
 
 	m_DefenseUp[Empire] = On;
 
-	// 군주 금강권 쿨타임 
 	m_DefenseUpCT[Empire] = thecore_pulse() + PASSES_PER_SEC(60 * 10);
 }
 
 bool IsMonarchWarpZone (int map_idx)
 {
-	// 아귀동굴, 천의동굴.
 	if (map_idx >= 10000)
 		map_idx /= 10000;
 
@@ -265,11 +258,12 @@ bool IsMonarchWarpZone (int map_idx)
 	case 302:
 	case 303:
 	case 304:
-		//던전
-	case 351: // 적룡성
-	case 352: // 백룡지성
+
+	case 351:
+	case 352:
 		return false;
 	}
 
 	return (map_idx != 208 && map_idx != 216 && map_idx != 217);
 }
+
