d35c6f460cd08229c90f7cfeae816ee01c1781d2
EclipseShade <priv.shadow.project@gmail.com>
2024-12-06 17:23:10 +0100

    Update event.cpp

--- mainline/game/event.cpp
+++ mainline/game/event.cpp
@@ -12,12 +12,10 @@ static ObjectPool<EVENT> event_pool;
 
 static CEventQueue cxx_q;
 
-/* 이벤트를 생성하고 리턴한다 */
 LPEVENT event_create_ex(TEVENTFUNC func, event_info_data* info, long when)
 {
 	LPEVENT new_event = NULL;
 
-	/* 반드시 다음 pulse 이상의 시간이 지난 후에 부르도록 한다. */
 	if (when < 1)
 		when = 1;
 
@@ -38,7 +36,6 @@ LPEVENT event_create_ex(TEVENTFUNC func, event_info_data* info, long when)
 	return (new_event);
 }
 
-/* 시스템으로 부터 이벤트를 제거한다 */
 void event_cancel(LPEVENT * ppevent)
 {
 	LPEVENT event;
@@ -63,7 +60,6 @@ void event_cancel(LPEVENT * ppevent)
 		return;
 	}
 
-	// 이미 취소 되었는가?
 	if (!event->q_el)
 	{
 		*ppevent = NULL;
@@ -92,14 +88,11 @@ void event_reset_time(LPEVENT event, long when)
 	}
 }
 
-/* 이벤트를 실행할 시간에 도달한 이벤트들을 실행한다 */
 int event_process(int pulse)
 {
 	long	new_time;
 	int		num_events = 0;
 
-	// event_q 즉 이벤트 큐의 헤드의 시간보다 현재의 pulse 가 적으면 루프문이 
-	// 돌지 않게 된다.
 	while (pulse >= cxx_q.GetTopKey())
 	{
 		TQueueElement * pElem = cxx_q.Dequeue();
@@ -116,11 +109,6 @@ int event_process(int pulse)
 		long processing_time = event_processing_time(the_event);
 		cxx_q.Delete(pElem);
 
-		/*
-		 * 리턴 값은 새로운 시간이며 리턴 값이 0 보다 클 경우 이벤트를 다시 추가한다. 
-		 * 리턴 값을 0 이상으로 할 경우 event 에 할당된 메모리 정보를 삭제하지 않도록
-		 * 주의한다.
-		 */
 		the_event->is_processing = TRUE;
 
 		if (!the_event->info)
@@ -132,7 +120,7 @@ int event_process(int pulse)
 		{
 			//sys_log(0, "EVENT: %s %d event %p info %p", the_event->file, the_event->line, the_event, the_event->info);
 			new_time = (the_event->func) (get_pointer(the_event), processing_time);
-			
+
 			if (new_time <= 0 || the_event->is_force_to_end)
 			{
 				the_event->q_el = NULL;
@@ -150,7 +138,6 @@ int event_process(int pulse)
 	return num_events;
 }
 
-/* 이벤트가 수행시간을 pulse 단위로 리턴해 준다 */
 long event_processing_time(LPEVENT event)
 {
 	long start_time;
@@ -162,7 +149,6 @@ long event_processing_time(LPEVENT event)
 	return (thecore_heart->pulse - start_time);
 }
 
-/* 이벤트가 남은 시간을 pulse 단위로 리턴해 준다 */
 long event_time(LPEVENT event)
 {
 	long when;
@@ -174,7 +160,6 @@ long event_time(LPEVENT event)
 	return (when - thecore_heart->pulse);
 }
 
-/* 모든 이벤트를 제거한다 */
 void event_destroy(void)
 {
 	TQueueElement * pElem;
@@ -210,3 +195,4 @@ void intrusive_ptr_release(EVENT* p) {
 #endif
 	}
 }
+
