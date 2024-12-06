e12f88355cd6f609362b39f58c43ffb2ae8f09f6
EclipseShade <priv.shadow.project@gmail.com>
2024-12-06 13:04:53 +0100

    Update item_manager.h

--- mainline/game/item_manager.h
+++ mainline/game/item_manager.h
@@ -5,9 +5,6 @@
 	#include "pool.h"
 #endif
 
-// special_item_group.txt에서 정의하는 속성 그룹
-// type attr로 선언할 수 있다.
-// 이 속성 그룹을 이용할 수 있는 것은 special_item_group.txt에서 Special type으로 정의된 그룹에 속한 UNIQUE ITEM이다.
 class CSpecialAttrGroup
 {
 public:
@@ -41,12 +38,11 @@ class CSpecialItemGroup
 			DRAIN_HP,
 			POISON,
 			MOB_GROUP,
+#ifdef ENABLE_WOLFMAN_CHARACTER
+			BLEEDING,
+#endif
 		};
 
-		// QUEST 타입은 퀘스트 스크립트에서 vnum.sig_use를 사용할 수 있는 그룹이다.
-		//		단, 이 그룹에 들어가기 위해서는 ITEM 자체의 TYPE이 QUEST여야 한다.
-		// SPECIAL 타입은 idx, item_vnum, attr_vnum을 입력한다. attr_vnum은 위에 CSpecialAttrGroup의 Vnum이다.
-		//		이 그룹에 들어있는 아이템은 같이 착용할 수 없다.
 		enum ESIGType { NORMAL, PCT, QUEST, SPECIAL };
 
 		struct CSpecialItemInfo
@@ -79,10 +75,6 @@ class CSpecialItemGroup
 			return m_vecProbs.empty();
 		}
 
-		// Type Multi, 즉 m_bType == PCT 인 경우,
-		// 확률을 더해가지 않고, 독립적으로 계산하여 아이템을 생성한다.
-		// 따라서 여러 개의 아이템이 생성될 수 있다.
-		// by rtsummit
 		int GetMultiIndex(std::vector <int> &idx_vec) const
 		{
 			idx_vec.clear();
@@ -142,9 +134,7 @@ class CSpecialItemGroup
 			}
 			return false;
 		}
-		
-		// Group의 Type이 Special인 경우에
-		// dwVnum에 매칭되는 AttrVnum을 return해준다.
+
 		DWORD GetAttrVnum(DWORD dwVnum) const
 		{
 			if (CSpecialItemGroup::SPECIAL != m_bType)
@@ -159,7 +149,6 @@ class CSpecialItemGroup
 			return 0;
 		}
 
-		// Group의 Size를 return해준다.
 		int GetGroupSize() const
 		{
 			return m_vecProbs.size();
@@ -360,18 +349,15 @@ class ITEM_MANAGER : public singleton<ITEM_MANAGER>
 
 		bool                    Initialize(TItemTable * table, int size);
 		void			Destroy();
-		void			Update();	// 매 루프마다 부른다.
+		void			Update();
 		void			GracefulShutdown();
 
 		DWORD			GetNewID();
-		bool			SetMaxItemID(TItemIDRangeTable range); // 최대 고유 아이디를 지정
+		bool			SetMaxItemID(TItemIDRangeTable range);
 		bool			SetMaxSpareItemID(TItemIDRangeTable range);
 
-		// DelayedSave: 어떠한 루틴 내에서 저장을 해야 할 짓을 많이 하면 저장
-		// 쿼리가 너무 많아지므로 "저장을 한다" 라고 표시만 해두고 잠깐
-		// (예: 1 frame) 후에 저장시킨다.
 		void			DelayedSave(LPITEM item);
-		void			FlushDelayedSave(LPITEM item); // Delayed 리스트에 있다면 지우고 저장한다. 끊김 처리시 사용 됨.
+		void			FlushDelayedSave(LPITEM item);
 		void			SaveSingleItem(LPITEM item);
 
 		LPITEM                  CreateItem(DWORD vnum, DWORD count = 1, DWORD dwID = 0, bool bTryMagic = false, int iRarePct = -1, bool bSkipSave = false);
@@ -380,7 +366,7 @@ class ITEM_MANAGER : public singleton<ITEM_MANAGER>
 #else
 		void DestroyItem(LPITEM item, const char* file, size_t line);
 #endif
-		void			RemoveItem(LPITEM item, const char * c_pszReason=NULL); // 사용자로 부터 아이템을 제거
+		void			RemoveItem(LPITEM item, const char * c_pszReason=NULL);
 
 		LPITEM			Find(DWORD id);
 		LPITEM                  FindByVID(DWORD vid);
@@ -396,15 +382,14 @@ class ITEM_MANAGER : public singleton<ITEM_MANAGER>
 		bool			ReadDropItemGroup(const char * c_pszFileName);
 		bool			ReadMonsterDropItemGroup(const char * c_pszFileName);
 		bool			ReadSpecialDropItemFile(const char * c_pszFileName);
-		
+
 		// convert name -> vnum special_item_group.txt
 		bool			ConvSpecialDropItemFile();
 		// convert name -> vnum special_item_group.txt
 
 		DWORD			GetRefineFromVnum(DWORD dwVnum);
 
-		static void		CopyAllAttrTo(LPITEM pkOldItem, LPITEM pkNewItem);		// pkNewItem으로 모든 속성과 소켓 값들을 목사하는 함수.
-
+		static void		CopyAllAttrTo(LPITEM pkOldItem, LPITEM pkNewItem);
 
 		const CSpecialItemGroup* GetSpecialItemGroup(DWORD dwVnum);
 		const CSpecialAttrGroup* GetSpecialAttrGroup(DWORD dwVnum);
@@ -420,15 +405,15 @@ class ITEM_MANAGER : public singleton<ITEM_MANAGER>
 		void			CreateQuestDropItem(LPCHARACTER pkChr, LPCHARACTER pkKiller, std::vector<LPITEM> & vec_item, int iDeltaPercent, int iRandRange);
 
 	protected:
-		typedef std::map<DWORD, LPITEM> ITEM_VID_MAP;		
+		typedef std::map<DWORD, LPITEM> ITEM_VID_MAP;
 
 		std::vector<TItemTable>		m_vec_prototype;
 		std::vector<TItemTable*> m_vec_item_vnum_range_info;
 		std::map<DWORD, DWORD>		m_map_ItemRefineFrom;
 		int				m_iTopOfTable;
 
-		ITEM_VID_MAP			m_VIDMap;			///< m_dwVIDCount 의 값단위로 아이템을 저장한다.
-		DWORD				m_dwVIDCount;			///< 이녀석 VID가 아니라 그냥 프로세스 단위 유니크 번호다.
+		ITEM_VID_MAP			m_VIDMap;
+		DWORD				m_dwVIDCount;
 		DWORD				m_dwCurrentID;
 		TItemIDRangeTable	m_ItemIDRange;
 		TItemIDRangeTable	m_ItemIDSpareRange;
@@ -447,26 +432,18 @@ class ITEM_MANAGER : public singleton<ITEM_MANAGER>
 		// CHECK_UNIQUE_GROUP
 		std::map<DWORD, int>		m_ItemToSpecialGroup;
 		// END_OF_CHECK_UNIQUE_GROUP
-	
+
 	private:
-		// 독일에서 기존 캐시 아이템과 같지만, 교환 가능한 캐시 아이템을 만든다고 하여,
-		// 오리지널 아이템에 교환 금지 플래그만 삭제한 새로운 아이템들을 만들어,
-		// 새로운 아이템 대역을 할당하였다.
-		// 문제는 새로운 아이템도 오리지널 아이템과 같은 효과를 내야하는데,
-		// 서버건, 클라건, vnum 기반으로 되어있어
-		// 새로운 vnum을 죄다 서버에 새로 다 박아야하는 안타까운 상황에 맞닿았다.
-		// 그래서 새 vnum의 아이템이면, 서버에서 돌아갈 때는 오리지널 아이템 vnum으로 바꿔서 돌고 하고,
-		// 저장할 때에 본래 vnum으로 바꿔주도록 한다.
-		// 이를 위해 오리지널 vnum과 새로운 vnum을 연결시켜주는 맵을 만듦.
+
 		typedef std::map <DWORD, DWORD> TMapDW2DW;
 		TMapDW2DW	m_map_new_to_ori;
 
-	public:	
+	public:
 		DWORD	GetMaskVnum(DWORD dwVnum);
 		std::map<DWORD, TItemTable>  m_map_vid;
 		std::map<DWORD, TItemTable>&  GetVIDMap() { return m_map_vid; }
-		std::vector<TItemTable>& GetVecProto() { return m_vec_prototype; }	
-		
+		std::vector<TItemTable>& GetVecProto() { return m_vec_prototype; }
+
 		const static int MAX_NORM_ATTR_NUM = 5;
 		const static int MAX_RARE_ATTR_NUM = 2;
 		bool ReadItemVnumMaskTable(const char * c_pszFileName);
@@ -483,3 +460,4 @@ class ITEM_MANAGER : public singleton<ITEM_MANAGER>
 #endif
 
 #endif
+
