ccee17d585a1fc7f1c0dcd0a2895d3e30fe505e7
EclipseShade <priv.shadow.project@gmail.com>
2024-12-06 09:19:37 +0100

    Update PetSystem.h

--- mainline/game/PetSystem.h
+++ mainline/game/PetSystem.h
@@ -1,17 +1,13 @@
 #ifndef	__HEADER_PET_SYSTEM__
 #define	__HEADER_PET_SYSTEM__
 
-
 class CHARACTER;
 
-// TODO: 펫으로서의 능력치? 라던가 친밀도, 배고픔 기타등등... 수치
 struct SPetAbility
 {
 };
 
-/**
-*/
-class CPetActor //: public CHARACTER
+class CPetActor
 {
 public:
 	enum EPetOptions
@@ -19,26 +15,21 @@ public:
 		EPetOption_Followable		= 1 << 0,
 		EPetOption_Mountable		= 1 << 1,
 		EPetOption_Summonable		= 1 << 2,
-		EPetOption_Combatable		= 1 << 3,		
+		EPetOption_Combatable		= 1 << 3,
 	};
-
 
 protected:
 	friend class CPetSystem;
 
 	CPetActor(LPCHARACTER owner, DWORD vnum, DWORD options = EPetOption_Followable | EPetOption_Summonable);
-//	CPetActor(LPCHARACTER owner, DWORD vnum, const SPetAbility& petAbility, DWORD options = EPetOption_Followable | EPetOption_Summonable);
 
 	virtual ~CPetActor();
 
 	virtual bool	Update(DWORD deltaTime);
 
 protected:
-	virtual bool	_UpdateFollowAI();				///< 주인을 따라다니는 AI 처리
-	virtual bool	_UpdatAloneActionAI(float fMinDist, float fMaxDist);			///< 주인 근처에서 혼자 노는 AI 처리
-
-	/// @TODO
-	//virtual bool	_UpdateCombatAI();
+	virtual bool	_UpdateFollowAI();
+	virtual bool	_UpdatAloneActionAI(float fMinDist, float fMaxDist);
 
 private:
 	bool Follow(float fMinDistance = 50.f);
@@ -60,15 +51,9 @@ public:
 	void			Unsummon();
 
 	bool			IsSummoned() const			{ return 0 != m_pkChar; }
-	void			SetSummonItem (LPITEM pItem);
+	void			SetSummonItem(LPITEM pItem);
 	DWORD			GetSummonItemVID () { return m_dwSummonItemVID; }
-	// 버프 주는 함수와 거두는 함수.
-	// 이게 좀 괴랄한게, 서버가 ㅄ라서, 
-	// POINT_MOV_SPEED, POINT_ATT_SPEED, POINT_CAST_SPEED는 PointChange()란 함수만 써서 변경해 봐야 소용이 없는게,
-	// PointChange() 이후에 어디선가 ComputePoints()를 하면 싹다 초기화되고, 
-	// 더 웃긴건, ComputePoints()를 부르지 않으면 클라의 POINT는 전혀 변하지 않는다는 거다.
-	// 그래서 버프를 주는 것은 ComputePoints() 내부에서 petsystem->RefreshBuff()를 부르도록 하였고,
-	// 버프를 빼는 것은 ClearBuff()를 부르고, ComputePoints를 하는 것으로 한다.
+
 	void			GiveBuff();
 	void			ClearBuff();
 
@@ -86,8 +71,6 @@ private:
 
 	LPCHARACTER		m_pkChar;					// Instance of pet(CHARACTER)
 	LPCHARACTER		m_pkOwner;
-
-//	SPetAbility		m_petAbility;				// 능력치
 };
 
 /**
@@ -95,7 +78,7 @@ private:
 class CPetSystem
 {
 public:
-	typedef	boost::unordered_map<DWORD,	CPetActor*>		TPetActorMap;		/// <VNUM, PetActor> map. (한 캐릭터가 같은 vnum의 펫을 여러개 가질 일이 있을까..??)
+	typedef	boost::unordered_map<DWORD,	CPetActor*>		TPetActorMap;
 
 public:
 	CPetSystem(LPCHARACTER owner);
@@ -107,7 +90,7 @@ public:
 	bool		Update(DWORD deltaTime);
 	void		Destroy();
 
-	size_t		CountSummoned() const;			///< 현재 소환된(실체화 된 캐릭터가 있는) 펫의 개수
+	size_t		CountSummoned() const;
 
 public:
 	void		SetUpdatePeriod(DWORD ms);
@@ -117,7 +100,6 @@ public:
 	void		Unsummon(DWORD mobVnum, bool bDeleteFromList = false);
 	void		Unsummon(CPetActor* petActor, bool bDeleteFromList = false);
 
-	// TODO: 진짜 펫 시스템이 들어갈 때 구현. (캐릭터가 보유한 펫의 정보를 추가할 때 라던가...)
 	CPetActor*	AddPet(DWORD mobVnum, const char* petName, const SPetAbility& ability, DWORD options = CPetActor::EPetOption_Followable | CPetActor::EPetOption_Summonable | CPetActor::EPetOption_Combatable);
 
 	void		DeletePet(DWORD mobVnum);
@@ -126,38 +108,11 @@ public:
 
 private:
 	TPetActorMap	m_petActorMap;
-	LPCHARACTER		m_pkOwner;					///< 펫 시스템의 Owner
-	DWORD			m_dwUpdatePeriod;			///< 업데이트 주기 (ms단위)
+	LPCHARACTER		m_pkOwner;
+	DWORD			m_dwUpdatePeriod;
 	DWORD			m_dwLastUpdateTime;
 	LPEVENT			m_pkPetSystemUpdateEvent;
 };
 
-/**
-// Summon Pet
-CPetSystem* petSystem = mainChar->GetPetSystem();
-CPetActor* petActor = petSystem->Summon(~~~);
-
-DWORD petVID = petActor->GetVID();
-if (0 == petActor)
-{
-	ERROR_LOG(...)
-};
-
-
-// Unsummon Pet
-petSystem->Unsummon(petVID);
-
-// Mount Pet
-petActor->Mount()..
-
-
-CPetActor::Update(...)
-{
-	// AI : Follow, actions, etc...
-}
-
-*/
-
-
-
 #endif	//__HEADER_PET_SYSTEM__
+
